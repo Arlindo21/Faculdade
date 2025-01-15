701320a9abb41c1546a360f4209fe54492cc6816
Arlindolima <Arlindolima307@gmail.com>
2025-01-15 12:14:22 -0300

    Pratica

---
+++ Módulo_1A Basic_FrontEnd/Desenvolvimento_Dinâmico /Mode_1/funcoes.js
@@ -0,0 +1,9 @@
+//Funções regulares 
+function soma(a,b){
+    return a+b;
+
+}
+//Chamando a  função e armazenado o resultado
+let resultado=soma(13,29);
+//Exibir o resultado 
+console.log(`O resultado da soma é ${resultado}`);
